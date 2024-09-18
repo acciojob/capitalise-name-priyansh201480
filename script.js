f6344fea07e6d185d392fe06041205657d8e5202
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-18 22:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const inputField = document.getElementById('fname');
+
+inputField.addEventListener('blur',function() {
+	inputField.value=inputField.value.toUpeerCase();
+});
+
+
+
